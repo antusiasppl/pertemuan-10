94ec5d0c8649e98a8e7c169e28daf59cce83fcfd
antusiasppl <ikhwanur30@gmail.com>
2024-06-13 14:57:17 +0700

    pertemuan 10

---
+++ praktikum10-3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+
+<?php
+ //Mendefinisikan variabel dengan berbagai macam tipe data
+ $nim = "21.11.0000";
+ $nama = "Feby Rastanti";
+ $umur = 21;
+ $nilai = 92.2;
+ $status = TRUE;
+ //Menampilkan data
+ echo "NIM : ".$nim."<br>";
+ echo "Nama : $nama <br>";
+ print "Umur : ".$umur."<br>";
+ printf ("Nilai : %.3f<br>",$nilai);
+ if($status)
+ echo "Status : Aktif";
+ else
+ echo "Status : Tidak Aktif";
+ ?>
---
+++ praktikum10-4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+
+<?php
+ $angka1=15;
+ $angka2=3;
+
+ $tambah = $angka1 + $angka2;
+ $kurang = $angka1 - $angka2;
+ $kali = $angka1 * $angka2;
+ $bagi = $angka1 / $angka2;
+ $modulus = $angka1 % $angka2;
+
+ echo "<ha2> perhitungan Aritmatika </h2>";
+ echo "$angka1 + $angka2 = $tambah";
+ echo "<br>$angka1 - $angka2 = $kurang";
+ echo "<br>$angka1 * $angka2 = $kali";
+ echo "<br>$angka1 / $angka2 = $bagi";
+ echo "<br>$angka1 % $angka2 = $modulus";
+ ?>
